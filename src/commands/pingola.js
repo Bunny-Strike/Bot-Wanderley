1a37e9da5e662242ea336d71f76bdc6059ef2acb
Allan Gabriel <allan.gabriel@protonmail.com>
2021-02-12 21:18:21 -0300

    Formata codigo

--- src/commands/pingola.js
+++ src/commands/pingola.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name:"pingola",
-    description: "Avalia o tamanho de sua pingola ao vivo",
-    execute(msg) {
-        const length = Math.floor(Math.random() * 100);
-        msg.reply(` voce tem ${length} cm de pingola`);
-    }
-}
+  name: "pingola",
+  description: "Avalia o tamanho de sua pingola ao vivo",
+  execute(msg) {
+    const length = Math.floor(Math.random() * 100);
+    msg.reply(` voce tem ${length} cm de pingola`);
+  },
+};
--- src/commands/reclamacao.js
+++ src/commands/reclamacao.js
@@ -1,7 +1,9 @@
 module.exports = {
-    name:"reclamacao",
-    description: "registra uma reclamação",
-    execute(msg) {
-        msg.reply(" sua reclamação foi encaminhada com sucesso ao nosso departamento do FODASE '-'");
-    }
-}
+  name: "reclamacao",
+  description: "registra uma reclamação",
+  execute(msg) {
+    msg.reply(
+      " sua reclamação foi encaminhada com sucesso ao nosso departamento do FODASE '-'"
+    );
+  },
+};
